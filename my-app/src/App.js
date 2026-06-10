b4eae1f7ed8fc442243f140fd9375e93cfd140cb
lalithakempula <lalithakempula@gmail.com>
2026-06-11 01:17:29 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Hello, I am Lalitha 👋</h1>
+        <h2>Aspiring React Developer</h2>
+        <p>I'm currently learning how to build awesome UIs with React!</p>
+        <p><strong>Fun Fact:</strong> I can debug in dreams 💻</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
